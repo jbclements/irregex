2bc7041f3b80db634fa868b4a4d55879c85e18a5
John Clements <clements@racket-lang.org>
2021-06-25 18:03:27 -0400

    add deps

--- info.rkt
+++ info.rkt
@@ -2,3 +2,7 @@
 
 (define collection 'use-pkg-name)
 (define scribblings '(("irregex.scrbl")))
+(define deps
+  (list "base" "rackunit-lib"))
+(define build-deps
+  (list "racket-doc" "scribble-lib"))
